028bfa57efd36cd2551aeefe6460ece332fd5a4f
adou600 <adrien.nicolet@gmail.com>
2014-05-13 16:18:02 +0200

    error management on drone service

--- client/angular/app/js/controllers.js
+++ client/angular/app/js/controllers.js
@@ -7,19 +7,34 @@ angular.module('myApp.controllers', [])
         $scope.commandProtocol = [{entry : "Nothing done yet."}];
 
         $scope.takeOff = function(){
-            addProtocolEntry("Taking off");
-            DroneService.takeOff();
+            addProtocolEntry("Take off command sent");
+            DroneService.takeOff(successCallBack, errorCallBack);
         };
 
         $scope.stop = function(){
-            addProtocolEntry("Stopping");
+            addProtocolEntry("Stop command sent");
+            DroneService.stop(successCallBack, errorCallBack);
         };
 
-        $scope.land = function(){
-            addProtocolEntry("Landing");
+        $scope.up = function(speed){
+            addProtocolEntry("Up command sent");
+            DroneService.up(successCallBack, errorCallBack, speed);
+        };
+
+        $scope.down = function(speed){
+            addProtocolEntry("Up command sent");
+            DroneService.down(successCallBack, errorCallBack, speed);
         };
 
         function addProtocolEntry(entryText){
             $scope.commandProtocol.splice(0,0, {entry: entryText});
+        }
+
+        function successCallBack(data) {
+            addProtocolEntry(data);
+        }
+
+        function errorCallBack(data) {
+            addProtocolEntry(data);
         }
   }]);
