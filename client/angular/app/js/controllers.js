1aa342a07fff6cbad3a75529e1ac8efc3ae1e792
Daniel Diehm <daniel.diehm@zuehlke.com>
2014-05-13 16:13:58 +0200

    First approach for usage of slider control.

--- client/angular/app/js/controllers.js
+++ client/angular/app/js/controllers.js
@@ -5,6 +5,7 @@
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope','DroneService', function($scope, DroneService) {
         $scope.commandProtocol = [{entry : "Nothing done yet."}];
+        $scope.verticalSpeed = 50;
 
         $scope.takeOff = function(){
             addProtocolEntry("Taking off");
@@ -19,7 +20,23 @@ angular.module('myApp.controllers', [])
             addProtocolEntry("Landing");
         };
 
+        $scope.verticalChange = function(){
+            if($scope.verticalSpeed > 55) {
+                //up
+                DroneService.up($scope.verticalSpeed / 100);
+            }else if($scope.verticalSpeed < 45) {
+                //down
+                DroneService.down($scope.verticalSpeed / 100);
+            }
+            else {
+                //stop
+                DroneService.stop();
+            }
+
+            $scope.commandProtocol.splice(0,1, {entry: $scope.verticalSpeed / 100});
+        }
+
         function addProtocolEntry(entryText){
-            $scope.commandProtocol.splice(0,0, {entry: entryText});
+            //$scope.commandProtocol.splice(0,1, {entry: entryText});
         }
   }]);
