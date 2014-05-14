35806b1598119cea78cce8ad09269db5e594411f
dom <dominic.wyler@zuehlke.com>
2014-05-14 11:56:42 +0200

    express server: send response after validation and before executing commands on the drone

--- express/server.js
+++ express/server.js
@@ -15,98 +15,98 @@ app.all('/*', function(req, res, next) {
 
 // Initiation
 app.get('/takeoff', function(req, res){
-    copterInstance.takeOff();
     res.send('OK');
+    copterInstance.takeOff();
 });
 
 app.get('/land', function(req, res){
-    copterInstance.land();
     res.send('OK');
+    copterInstance.land();
 });
 
 app.get('/stop', function(req, res){
-    copterInstance.stop();
     res.send('OK');
+    copterInstance.stop();
 });
 
 
 // Standard Moves
 app.get('/up/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.up(speed, 1000);
     res.send('OK');
+    copterInstance.up(speed, 1000);
 });
 
 app.get('/up/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.up(speed, duration);
     res.send('OK');
+    copterInstance.up(speed, duration);
 });
 
 app.get('/down/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.down(speed, 1000);
     res.send('OK');
+    copterInstance.down(speed, 1000);
 });
 
 app.get('/down/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.down(speed, duration);
     res.send('OK');
+    copterInstance.down(speed, duration);
 });
 
 app.get('/left/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.left(speed, 1000);
     res.send('OK');
+    copterInstance.left(speed, 1000);
 });
 
 app.get('/left/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.left(speed, duration);
     res.send('OK');
+    copterInstance.left(speed, duration);
 });
 
 app.get('/right/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.right(speed, 1000);
     res.send('OK');
+    copterInstance.right(speed, 1000);
 });
 
 app.get('/right/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.right(speed, duration);
     res.send('OK');
+    copterInstance.right(speed, duration);
 });
 
 app.get('/front/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.front(speed, 1000);
     res.send('OK');
+    copterInstance.front(speed, 1000);
 });
 
 app.get('/front/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.front(speed, duration);
     res.send('OK');
+    copterInstance.front(speed, duration);
 });
 
 app.get('/back/speed/:speed', function(req, res){
     var speed = validator.validateSpeed(req, res);
-    copterInstance.back(speed, 1000);
     res.send('OK');
+    copterInstance.back(speed, 1000);
 });
 
 app.get('/back/speed/:speed/duration/:duration', function(req, res){
     var speed = validator.validateSpeed(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.back(speed, duration);
     res.send('OK');
+    copterInstance.back(speed, duration);
 });
 
 
@@ -139,8 +139,8 @@ app.get('/turn/direction/:direction/angle/:angle/altitude/:altitude', function(r
 app.get('/animate/:animation/duration/:duration', function(req, res){
     var animation = validator.validateAnimation(req, res);
     var duration = validator.validateDuration(req, res);
-    copterInstance.animate(animation, duration);
     res.send('OK');
+    copterInstance.animate(animation, duration);
 });
 
 
